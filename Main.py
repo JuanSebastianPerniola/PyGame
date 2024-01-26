d056e821110780e98e7a1703b3c0959bfb913b9c
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-01-26 15:19:25 +0100

    Pygame

---
+++ Main.py
@@ -0,0 +1,191 @@
+import random
+import pygame
+import Fondo
+import Planeta
+import math
+import pygame_menu
+from pygame.locals import QUIT, KEYDOWN, K_SPACE, K_p, K_ESCAPE
+
+# Iniciar pygame y el método Main
+pygame.init()
+
+# Inicializar la pantalla
+tamaño = (800, 800)
+pantalla = pygame.display.set_mode(tamaño)
+pygame.display.set_caption("Invasion a planeta")
+font = pygame.font.Font(None, 25)
+
+
+# Grupos de sprites
+bullets_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+enemigos = pygame.sprite.Group()
+
+# Creación de Enemigo y añadirlo al grupo de sprites
+Enemigo = Planeta.Enemigo((0,0))
+fondo = Fondo.Fondo((0, 0))
+planeta = Planeta.Planeta((320, 360))
+bullet = Planeta.Bullet(0, 0, 0)
+enemigos.add(Enemigo)
+all_sprites.add(fondo, planeta, bullet)
+
+# Posición de vidas y puntaje en pantalla
+vidasX, vidasY = 10, 40
+font_vida = pygame.font.Font(None, 32)
+
+score_value = 0
+font_score = pygame.font.Font(None, 32)
+textX, textY = 10, 10
+
+
+
+planeta.set_difficulty(1)
+       
+       
+# Función para mostrar el puntaje en pantalla
+def show_score(x, y):
+    score = font_score.render("Puntuación: " + str(score_value), True, (255, 255, 255))
+    pantalla.blit(score, (x, y))
+
+# Función para mostrar las vidas en pantalla
+def vidas(x, y):
+    vidas = font_vida.render("Vidas: " + str(vidas_restantes), True, (255, 255, 255))
+    pantalla.blit(vidas, (x, y))
+
+
+# Función principal del juego
+def start_the_game():
+    global score_value, vidas_iniciales, frecuencia_enemigos, vidas_restantes, pausado
+
+    reloj = pygame.time.Clock()
+    FPS = 60
+    running = True
+    pausado = False
+    vidas_restantes = planeta.vidas_iniciales  # Utiliza las vidas_iniciales del objeto planeta
+    dificultad = planeta.frecuencia_enemigos  # Utiliza la frecuencia_enemigos del objeto planeta
+    reinicio_paritda = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                # if event.key == K_SPACE:
+                    # Crear una nueva bala en la dirección actual del jugador
+                if event.key == K_p:
+                    pausado = not pausado  # Cambiar el estado de pausado
+                    
+                # elif event.key == K_ESCAPE: 
+                    # if pausado:
+                        # pausado = False
+                        # menu.mainloop(pantalla)
+
+        # Obtener las teclas presionadas
+        keys = pygame.key.get_pressed()
+        planeta.movement(keys, planeta, bullets_group, all_sprites)       
+       
+
+        if not pausado:
+            print(vidas_restantes)
+            print(vidas_restantes)
+            pantalla.fill((255, 255, 255))
+
+            # Dibuja la imagen de fondo antes de actualizar la pantalla
+            pantalla.blit(background_image, (0, 0))
+
+            # Crear enemigos
+            if random.randint(0, 300) < dificultad:
+                
+                random_x = random.randint(-200, pantalla.get_width() + 200)
+                random_y = random.randint(-200, pantalla.get_height() + 200)
+                distancia_al_planeta = math.sqrt((random_x - planeta.rect.centerx) ** 2 + (random_y - planeta.rect.centery) ** 2)
+                #me daba error y me quitaba una vida de gratis 
+                # Verificar si la distancia al planeta es mayor que la distancia mínima permitida
+                distancia_minima_al_planeta = 500
+                if distancia_al_planeta > distancia_minima_al_planeta:
+                    nuevo_enemigo = Planeta.Enemigo((random_x, random_y))
+                    all_sprites.add(nuevo_enemigo)
+                    enemigos.add(nuevo_enemigo)
+              
+            # Actualizar y dibujar los sprites
+            all_sprites.update()
+            all_sprites.draw(pantalla)
+
+            # Mover los enemigos hacia el planeta
+            for enemigo in enemigos:
+                enemigo.move_towards_planet(planeta)
+
+            # Eliminar balas fuera de la pantalla
+            bullets_group.update()
+            bullets_group.draw(pantalla)
+
+            # Colisiones de balas con enemigos
+            for bala in bullets_group:
+                for enemigo in enemigos:
+                    if pygame.sprite.collide_rect(bala, enemigo):
+                        enemigos.remove(enemigo)
+                        all_sprites.remove(enemigo)
+                        bala.kill()
+                        score_value += 1
+
+            # Colisión de enemigos con el planeta
+            for enemigo in enemigos:
+                if pygame.sprite.collide_rect(enemigo, planeta):
+                    vidas_restantes -= 1
+                    enemigos.remove(enemigo)
+                    all_sprites.remove(enemigo)
+
+            # Verificar si el jugador se quedó sin vidas
+            if vidas_restantes <= 0:
+                if not reinicio_paritda:
+                    reinicio_paritda = pygame.time.get_ticks()
+                    pantalla.fill((0, 0, 0))
+                    texto_game_over = pygame.image.load("gameover.png")
+                    x = (pantalla.get_width() - texto_game_over.get_width()) // 2
+                    y = (pantalla.get_height() - texto_game_over.get_height()) // 2
+                    pantalla.blit(texto_game_over, (x, y))
+                    pygame.display.flip()
+                    pygame.time.delay(1500)
+                    reinicio_paritda = True
+                elif pygame.time.get_ticks() - reinicio_paritda >= 2000:
+                    vidas_restantes = planeta.vidas_iniciales
+                    score_value = 0
+                    for enemigo in enemigos:
+                        enemigo.kill()  # Elimina el sprite del grupo enemigos y all_sprites
+                    enemigos.empty()  # Vacía el grupo enemigos (también puedes omitir esto si usas enemigo.kill())
+                    pausado = False
+                    reinicio_paritda = False
+                    # Volver al menú principal
+                    menu.mainloop(pantalla)
+                                
+        if pausado:
+            texto = font.render("PAUSA", True, "White") 
+            pantalla.blit(texto,(pantalla.get_width()/2-30, pantalla.get_height()/2-200))
+        # Mostrar vidas y puntaje en pantalla
+        vidas(vidasX, vidasY)
+        show_score(textX, textY)
+
+        # Mostrar mensaje de pausa si el juego está pausado
+        
+        pygame.display.flip()
+        reloj.tick(FPS)
+
+# Carga la imagen que deseas agregar al fondo
+background_image_path = 'background.jpg'
+background_image = pygame.image.load(background_image_path)
+background_image = pygame.transform.scale(background_image, tamaño)
+
+# Crea el menú
+menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_DARK)
+
+# Agrega un cuadro de texto, un selector y dos botones como en tu código original
+menu.add.text_input('Name:', default='-----')
+menu.add.selector('Difficulty:', [('Hard', 1), ('Easy', 2)], onchange=planeta.set_difficulty)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+# Ejecuta el menú
+menu.mainloop(pantalla)
+
+# Salir del juego
+pygame.quit()
+
---
+++ Planeta.py
@@ -0,0 +1,101 @@
+import pygame
+import math
+import random
+
+# Clase balas
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, angle):
+        super().__init__()
+        imagen = pygame.image.load("venus.png")
+        self.image = pygame.transform.scale(imagen, (10, 10))
+        self.Imagen = self.image
+        self.rect = self.image.get_rect(center=(x, y))
+        self.angle = angle
+
+    def update(self):
+        rad_angle = math.radians(self.angle)
+        self.rect.x += 5 * math.cos(rad_angle)
+        self.rect.y += 5 * math.sin(rad_angle)
+        # pantalla.blit(self.image, self.rect.topleft)
+class Planeta(pygame.sprite.Sprite):
+    def __init__(self, posicion):
+        super().__init__()
+        imagen = pygame.image.load("venus.png")
+        self.image = pygame.transform.scale(imagen, (150, 150))
+        self.Imagen = self.image
+        self.rect = self.image.get_rect()  # Ya no es necesario, pero lo dejaremos para compatibilidad
+        self.rect.topleft = posicion
+        self.angle = 0
+        self.bullets = pygame.sprite.Group()
+        self.mask = pygame.mask.from_surface(self.image)
+        vidas_iniciales = 3
+        frecuencia_enemigos = 5
+        self.shoot_cooldown = 50  # Cooldown en milisegundos (1.2 segundos)
+        self.last_shot_time = 0 
+        
+    def movement(self, keys, planeta, bullets_group, all_sprites):
+        if keys[pygame.K_LEFT]:
+            planeta.angle += 2
+        if keys[pygame.K_RIGHT]:
+            planeta.angle -= 2 
+        if keys[pygame.K_SPACE]:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.last_shot_time > self.shoot_cooldown:
+                self.shoot(bullets_group, all_sprites)
+                self.last_shot_time = current_time
+
+    def shoot(self, bullets_group, all_sprites):
+        nueva_bala = Bullet(self.rect.centerx, self.rect.centery, self.angle)
+        bullets_group.add(nueva_bala)
+        all_sprites.add(nueva_bala)
+        
+        
+    def set_difficulty(self, dificultad):
+        if dificultad == 1:
+            Planeta.vidas_iniciales = 3
+            Planeta.frecuencia_enemigos = 4.5
+        elif dificultad == 2:
+            Planeta.vidas_iniciales = 5
+            Planeta.frecuencia_enemigos = 3
+    def update(self):
+        # # si las ponemos asi tambien se mueve (experimentos)
+        self.mask =  pygame.mask.from_surface(self.image)
+        self.image = pygame.transform.rotate(self.Imagen, -self.angle)
+        self.mask = pygame.mask.from_surface(self.image)
+        # Actualiza la posición del rectángulo si es necesario
+        self.rect = self.image.get_rect(center=self.rect.center)
+    def draw(self, screen):
+            screen.blit(self.image, self.rect.topleft)
+            
+class Enemigo(pygame.sprite.Sprite):
+    def __init__(self, posicion):
+        super().__init__()
+        imagen = pygame.image.load("alien.png")
+        self.image = pygame.transform.scale(imagen, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = posicion
+        self.speed = 2  # Velocidad de movimiento del enemigo
+        self.hitbox = (self.rect.x + 17, self.rect.y + 2, 31, 57) 
+        self.frecuencia = 0
+        
+    def move_towards_planet(self, planeta):
+        # Calcular el ángulo entre el enemigo y el planeta
+        angle = math.atan2(planeta.rect.centery - self.rect.centery, planeta.rect.centerx - self.rect.centerx)
+        # Actualizar las coordenadas del enemigo en dirección al planeta
+        self.rect.x += self.speed * math.cos(angle)
+        self.rect.y += self.speed * math.sin(angle)
+        self.enmigo_posicin = self.rect.y and self.rect.x
+        pass
+    
+    # def update(self):
+    def set_difficulty(vidas, dificultad):
+        # global vidas_iniciales, frecuencia_enemigos
+        if dificultad == 1: 
+            # vidas = 3  # difícil
+            dificultad = 5
+            # frecuencia_enemigos = dificultad
+        elif dificultad == 2:  # fácil
+            # vidas = 5  # difícil
+            # vidas_iniciales = vidas
+            dificultad = 3
+            # frecuencia_enemigos = dificultad
